6bcda787d1da85ab5afff811b2a7ccecb5e20120
inanamm <martina.holenstein@gmail.com>
2023-06-25 19:09:24 +0200

    about page setup

--- site/templates/about.php
+++ site/templates/about.php
@@ -5,12 +5,59 @@
 
 <body>
 	<?php snippet('header') ?>
-	<main>
-		<?php foreach ($page->myBlocksField()->toBlocks() as $block) : ?>
-			<div id="<?= $block->id() ?>" class="block block-type-<?= $block->type() ?>">
-				<?= $block ?>
+	<main class="text-csblue">
+		<h1 class="text-5xl mb-8">logo co–struct</h1>
+		<div class="flex flex-col divide-y divide-csblue">
+			<div class="px-3 pt-2 pb-5">
+				<p class="description">
+					co–struct ist ein inhabergeführtes Büro für Tragwerksplanung in Zürich und Aubonne, gegründet von Fabrice Meylan und Sam Bouten.
+					Das Ziel ist, innovative Tragsysteme von der Entstehung des Projekts an zu planen und dabei ein besonderes Augenmerk auf ästhetische Aspekte zu legen. Die enge Zusammenarbeit mit den verschiedenen Partner:innen, sowie die proaktive Optimierung der erdachten Strukturen liegen co–struct am Herzen. Die Ökologie im Bauwesen regt unsere Kreativität an, um für jedes Bauwerk die am besten geeigneten Materialien zu finden.
+				</p>
 			</div>
-		<?php endforeach ?>
+			<div class="px-3 pt-2 pb-5">
+				<p class="address">
+					co–struct AG
+					Tragwerksplanung
+					Förrlibuckstrasse 225
+					CH—8005 Zürich
+
+					co–struct AG
+					Ingénierie structurale
+					Chemin des Clos 17
+					CH—1170 Aubonne
+
+					+41 44 506 66 30
+					<a href="mailto:hello@martinaholenstein.ch">Email</a>
+					<a href="https://www.instagram.com/inanamm/" target="_blank">Instagram</a>
+				</p>
+			</div>
+			<div class="px-3 pt-2 pb-5">
+				<div class="collapse collapse-plus">
+					<input type="checkbox" />
+					<div class="collapse-title">
+						Fabrice Meylan
+					</div>
+					<div class="collapse-content">
+						<p>+41 44 506 66 30<br>
+							fabrice@co–struct.ch</p>
+						<div class="w-40 aspect-[4/5] bg-csblue opacity-10"></div>
+					</div>
+				</div>
+				<div class="collapse collapse-plus">
+					<input type="checkbox" />
+					<div class="collapse-title">
+						Sam Bouten
+					</div>
+					<div class="collapse-content">
+						<p>+41 44 506 66 30<br>
+							sam@co–struct.ch</p>
+						<div class="w-40 aspect-[4/5] bg-csblue opacity-10"></div>
+					</div>
+				</div>
+			</div>
+
+			<div class="px-3 pt-2 pb-5">hello</div>
+		</div>
 	</main>
 	<?php snippet('footer') ?>
 </body>
