97a70646eb2353f3d56ffa95b7049e6c56b21abe
Guido Offermans <dev@guidooffermans.com>
2023-07-29 15:48:46 +0200

    add projects overview on site

---
+++ site/templates/project.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<?php snippet('head') ?>
+
+<body>
+    <?php snippet('header') ?>
+    <main class="text-csblue">
+
+
+        
+
+
+    </main>
+    <div class="text-csblue">
+        <?php snippet('footer') ?>
+    </div>
+
+</body>
+
+
+</html>
