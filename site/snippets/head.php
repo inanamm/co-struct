ddece0d71034baf8065a5444e732e8fc426f426d
Guido Offermans <dev@guidooffermans.com>
2023-08-01 16:08:02 +0200

    add SEO plugin

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -3,15 +3,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
 
-    <meta name="title" content="co–struct Tragwerksplanung, ingénierie structurale">
-    <meta name="description" content="co–struct ist ein junges Tragwerkstatik Büro in Zürich und in Aubonne, Schweiz. Die Zusammenarbeit und das Detail stehen bei uns im Vordergrund.">
+    <?php snippet('meta_information'); ?>
+    <?php snippet('robots'); ?>
+
     <meta name="keywords" content="Tragwerkstatik, ingénierie structurale, structural engineering, schweiz, switzerland, suisse, Zürich, Aubonne, Architektur">
-    <meta name="robots" content="index, follow">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="language" content="Deutsch, Francais">
-    <meta name="author" content="co–struct, Fabrice Meylan">
-
-    <title><?= $site->title() ?></title>
 
     <?= vite()->css() ?>
 </head>
