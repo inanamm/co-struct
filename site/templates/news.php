4cbdab05c0eaf7289bc8ca1f6f21f2aca59533a2
Guido Offermans <dev@guidooffermans.com>
2023-08-03 21:39:20 +0200

    news pews

--- site/templates/news.php
+++ site/templates/news.php
@@ -7,22 +7,25 @@
     <?php snippet('header') ?>
 
     <main class="text-csgreen">
-        <div class="divide-y border-b border-csgreen">
-            <div class="flex flex-col px-3 ">
+        <div class=" border-b border-csgreen">
+
+            <div class="flex flex-col divide-y divide-csgreen ">
                 <?php if ($news = $page->news()->toStructure()) : ?>
                     <?php foreach ($news as $newsArticle) : ?>
 
-                        <div class="flex flex-col gap-1 pb-6">
+                        <div class="flex flex-col gap-1 pb-6 px-3 py-3">
                             <?php foreach ($newsArticle->image()->toFiles() as $image) : ?>
-                                <img src="<?= $image->url() ?>" class="aspect-[16/9] w-full h-auto">
+                                <img src="<?= $image->url() ?>" class="aspect-[16/9] w-full h-auto pt-4">
                             <?php endforeach ?>
 
                             <div class="mb-1 flex justify-between">
                                 <h2 class="text-lg"><?= $newsArticle->title()->toHtml() ?></h2>
-                                
+
                             </div>
-                            <p class="font-mono text-sm"><?= $newsArticle->date()->toHtml() ?> — 
-                            <?= $newsArticle->description()->toHtml() ?></p>
+                            <p class="font-mono text-sm">
+                                <?= $newsArticle->date()->toHtml() ?> —
+                                <?= $newsArticle->description()->toHtml() ?>
+                            </p>
                         </div>
 
                     <?php endforeach ?>
