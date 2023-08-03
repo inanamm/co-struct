66191947c300e95718ac8c50429b6062ae2b36fa
Guido Offermans <dev@guidooffermans.com>
2023-08-03 19:18:25 +0200

    new changes

--- site/snippets/newsarticle.php
+++ site/snippets/newsarticle.php
@@ -1,23 +1,19 @@
-<?php foreach( $page->news()->toStructure() as $newsArticle) : ?>
-    
-    <?php snippet(
-        'accordion',
-        ['buttonText' => $newsArticle->title()],
-        slots: true
-    )
-    ?>
-    <?php slot() ?>
-    <div class="flex flex-col gap-1 pb-6">
-        <p class="mb-1"><?= $newsArticle->title() ?><br>
-            <?= $newsArticle->description() ?>
-            <?= $newsArticle->date() ?>
-        </p>
+<?php foreach ($news as $newsArticle) : ?>
+
+
+    <div class="flex flex-col gap-1 pb-6 w-1/2">
 
         <?php foreach ($newsArticle->image()->toFiles() as $image) : ?>
-            <img src="<?= $image->crop(400)->url() ?>" class="aspect-[4/5] w-1/4 h-auto">
+            <img src="<?= $image->url() ?>" class="aspect-[16/9] w-full h-auto">
         <?php endforeach ?>
 
+        <h2 class="mb-1 flex justify-between">
+            <span class="text-lg"><?= $newsArticle->title()->toHtml() ?></span>
+            <i><?= $newsArticle->date()->toHtml() ?></i>
+        </h2>
+        <p><?= $newsArticle->description()->toHtml() ?></p>
+
+
     </div>
-    <?php endslot() ?>
-    <?php endsnippet() ?>
+
 <?php endforeach ?>
--- site/templates/news.php
+++ site/templates/news.php
@@ -5,43 +5,32 @@
 
 <body>
     <?php snippet('header') ?>
+
     <main class="text-csgreen">
-        <div class="flex flex-col divide-y divide-csgreen">
 
-            <?php snippet(
-                'article',
-                [
-                    'articleTitle' => 'co–struct gewinnt den 1. Preis!'
-                ],
-                slots: true
-            ) ?>
-                <?php slot('article') ?>
-                    <p>Hello you dumbo</p>
-                <?php endslot() ?>
-            <?php endsnippet() ?>
- 
- 
-            <?php snippet(
-                'article',
-                [
-                    'articleTitle' => 'co–struct gewinnt den 2. Preis!',
-                ],
-                slots: true
-            ) ?>
-                <?php slot('article') ?>
-                    <p>Hello you dumbo nr 2</p>
-                <?php endslot() ?>
-            <?php endsnippet() ?>
+        <div class="flex flex-col px-3">
+            <?php if ($news = $page->news()->toStructure()) : ?>
+                <?php foreach ($news as $newsArticle) : ?>
 
+                    <div class="flex flex-col gap-1 pb-6 w-1/2">
+                        <?php foreach ($newsArticle->image()->toFiles() as $image) : ?>
+                            <img src="<?= $image->url() ?>" class="aspect-[16/9] w-full h-auto">
+                        <?php endforeach ?>
 
+                        <div class="mb-1 flex justify-between">
+                            <h2 class="text-xl"><?= $newsArticle->title()->toHtml() ?></h2>
+                            <p><i><?= $newsArticle->date()->toHtml() ?></i></p>
+                        </div>
 
+                        <p><?= $newsArticle->description()->toHtml() ?></p>
+                    </div>
 
-        <?php snippet('newsarticle') ?>
-
-
-
+                <?php endforeach ?>
+            <?php endif ?>
         </div>
+
     </main>
+
     <div class="text-csgreen">
         <?php snippet('footer') ?>
     </div>
