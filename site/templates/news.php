e9b51eb5e02b23450b93dc0b9f749d291c708367
inanamm <martina.holenstein@gmail.com>
2023-08-03 20:54:15 +0200

    newsstuff

--- site/templates/news.php
+++ site/templates/news.php
@@ -7,28 +7,28 @@
     <?php snippet('header') ?>
 
     <main class="text-csgreen">
+        <div class="divide-y border-b border-csgreen">
+            <div class="flex flex-col px-3 ">
+                <?php if ($news = $page->news()->toStructure()) : ?>
+                    <?php foreach ($news as $newsArticle) : ?>
 
-        <div class="flex flex-col px-3">
-            <?php if ($news = $page->news()->toStructure()) : ?>
-                <?php foreach ($news as $newsArticle) : ?>
+                        <div class="flex flex-col gap-1 pb-6">
+                            <?php foreach ($newsArticle->image()->toFiles() as $image) : ?>
+                                <img src="<?= $image->url() ?>" class="aspect-[16/9] w-full h-auto">
+                            <?php endforeach ?>
 
-                    <div class="flex flex-col gap-1 pb-6 w-1/2">
-                        <?php foreach ($newsArticle->image()->toFiles() as $image) : ?>
-                            <img src="<?= $image->url() ?>" class="aspect-[16/9] w-full h-auto">
-                        <?php endforeach ?>
-
-                        <div class="mb-1 flex justify-between">
-                            <h2 class="text-xl"><?= $newsArticle->title()->toHtml() ?></h2>
-                            <p><i><?= $newsArticle->date()->toHtml() ?></i></p>
+                            <div class="mb-1 flex justify-between">
+                                <h2 class="text-lg"><?= $newsArticle->title()->toHtml() ?></h2>
+                                
+                            </div>
+                            <p class="font-mono text-sm"><?= $newsArticle->date()->toHtml() ?> — 
+                            <?= $newsArticle->description()->toHtml() ?></p>
                         </div>
 
-                        <p><?= $newsArticle->description()->toHtml() ?></p>
-                    </div>
-
-                <?php endforeach ?>
-            <?php endif ?>
+                    <?php endforeach ?>
+                <?php endif ?>
+            </div>
         </div>
-
     </main>
 
     <div class="text-csgreen">
