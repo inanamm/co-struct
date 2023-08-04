2c6c3e6ad46013363214920c193b9499adc8ae7e
inanamm <martina.holenstein@gmail.com>
2023-08-04 14:13:08 +0200

    home page projects grid

---
+++ site/templates/home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html class="bg-cswhite" lang="de">
+
+<?php snippet('head') ?>
+
+<body>
+    <?php snippet('header') ?>
+    <main class="text-csblack">
+        <div class=" border-b border-csblack">
+
+
+
+            <div class="flex flex-col divide-y divide-csblack ">
+
+                <div class="flex flex-col gap-4 font-sans text-base px-3 pt-2 pb-5">
+                    <?= $site->intro() ?>
+                </div>
+
+                <div class="grid grid-cols-2 px-3 pt-3 pb-5 gap-1 w-full">
+                    <?php foreach ($pages->get('projects')->children() as $project) : ?>
+                        <a href="<?= $project->url() ?>" class="pb-5">
+                            <img src="<?= $project->images()->findBy("template", "cover")->url() ?>" />
+                            <p class="font-mono text-xs"><?= $project->title() ?></p>
+                        </a>
+                    <?php endforeach ?>
+                </div>
+
+
+            </div>
+
+
+        </div>
+    </main>
+    <div class="text-csblack">
+        <?php snippet('footer') ?>
+    </div>
+    <?= vite()->js() ?>
+</body>
+
+</html>
