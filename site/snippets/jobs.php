91ca6e44431a22b1c15ef8c1da0597db96a49408
inanamm <martina.holenstein@gmail.com>
2023-08-16 11:48:35 +0200

    about page tryout and jobs

--- site/snippets/jobs.php
+++ site/snippets/jobs.php
@@ -8,11 +8,17 @@
     <?php slot() ?>
     <div class="pb-6">
         <div class="flex flex-col gap-1 pt-1 font-mono text-sm">
-            <p><?= $job->descriptionA() ?></p>
-            <p><?= $job->responsabilities()->list() ?></p>
-            <p><?= $job->profile()->list() ?></p>
-            <p><?= $job->descriptionB()->list() ?></p>
-            <p><?= $job->descriptionC() ?></p>
+            <p><?= $job->descriptionA()->kt() ?></p>
+            <ul class="">
+                <?php foreach ($job->responsabilities()->list() as $responsability) : ?>
+                    <li>
+                        <?= $responsability ?>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+            <p><?= $job->profile()->kt() ?></p>
+            <p><?= $job->descriptionB()->kt() ?></p>
+            <p><?= $job->descriptionC()->kt() ?></p>
         </div>
     </div>
     <?php endslot() ?>
