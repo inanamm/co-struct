fa45831260701d94913dbf249eff815acff59661
Guido Offermans <dev@guidooffermans.com>
2023-08-18 16:44:32 +0200

    set the first breakpoints for about page

--- site/templates/about.php
+++ site/templates/about.php
@@ -6,82 +6,87 @@
 <body class="text-csblue">
 	<?php snippet('header') ?>
 
-	<main class="flex flex-col divide-y divide-csblue border-b border-csblue">
+	<main class="flex flex-col lg:flex-row divide-y divide-csblue lg:divide-none border-b border-csblue">
 
-		<article class="flex flex-col gap-5 font-sans text-lg px-3 pt-2 pb-5">
+		<article class="flex flex-col gap-5 font-sans text-lg px-3 pt-2 pb-5 lg:w-1/2">
 			<?= $page->Bueroprofil()->kt() ?>
 		</article>
 
-		<div class="flex flex-col gap-4 font-sans text-lg px-3 pt-2 pb-5">
-			<?php if ($address = $site->addressZH()->toObject()) : ?>
-				<?php snippet(
-					'address',
-					[
-						'companyName' => $address->companyName()->escape(),
-						'description' => $address->description()->escape(),
-						'street' => $address->street()->escape(),
-						'postalCode' => $address->postalCode()->escape(),
-						'place' => $address->place()->escape(),
-					]
-				) ?>
-			<?php endif ?>
+		<div class="lg:w-1/2 lg:divide-none divide-y divide-csblue">
+			<div class="flex flex-col lg:flex-row gap-4 font-sans text-lg px-3 pt-2 pb-5">
+				<div class="flex flex-col gap-4">
+					<?php if ($address = $site->addressZH()->toObject()) : ?>
+						<?php snippet(
+							'address',
+							[
+								'companyName' => $address->companyName()->escape(),
+								'description' => $address->description()->escape(),
+								'street' => $address->street()->escape(),
+								'postalCode' => $address->postalCode()->escape(),
+								'place' => $address->place()->escape(),
+							]
+						) ?>
+					<?php endif ?>
 
-			<?php if ($address = $site->addressVD()->toObject()) : ?>
-				<?php snippet(
-					'address',
-					[
-						'companyName' => $address->companyName()->escape(),
-						'description' => $address->description()->escape(),
-						'street' => $address->street()->escape(),
-						'postalCode' => $address->postalCode()->escape(),
-						'place' => $address->place()->escape(),
-					]
-				) ?>
-			<?php endif ?>
-
-			<div class="flex flex-col gap-0">
-				<?php snippet('contact', ['phone' => $site->phone()->escape(), 'email' => $site->email()->escape()]) ?>
-				<?php snippet('link', [
-					'url' => $site->instagram(),
-					'description' => 'Instagram',
-					'blank' => true
-				]) ?>
-				<?php snippet('link', [
-					'url' => $site->linkedin(),
-					'description' => 'LinkedIn',
-					'blank' => true
-				]) ?>
+					<?php if ($address = $site->addressVD()->toObject()) : ?>
+						<?php snippet(
+							'address',
+							[
+								'companyName' => $address->companyName()->escape(),
+								'description' => $address->description()->escape(),
+								'street' => $address->street()->escape(),
+								'postalCode' => $address->postalCode()->escape(),
+								'place' => $address->place()->escape(),
+							]
+						) ?>
+					<?php endif ?>
+				</div>
+				<div class="flex flex-col gap-0 lg:order-first">
+					<?php snippet('contact', ['phone' => $site->phone()->escape(), 'email' => $site->email()->escape()]) ?>
+					<?php snippet('link', [
+						'url' => $site->instagram(),
+						'description' => 'Instagram',
+						'blank' => true
+					]) ?>
+					<?php snippet('link', [
+						'url' => $site->linkedin(),
+						'description' => 'LinkedIn',
+						'blank' => true
+					]) ?>
+				</div>
 			</div>
-		</div>
 
-		<div class="font-sans text-base px-3 pt-2 pb-5">
-			<?php snippet('subtitle', ['subtitle' => 'Mitarbeitende']) ?>
+			<div class="flex flex-col lg:flex-row lg:gap-5 font-sans text-base px-3 pt-2 pb-5">
+				<?php snippet('subtitle', ['subtitle' => 'Mitarbeitende']) ?>
 
-			<?php if ($coworkers = $page->coworkers()->toStructure()) : ?>
-				<?php snippet('coworkers', ['coworkers' => $coworkers]) ?>
-			<?php endif ?>
-		</div>
+				<div>
+					<?php if ($coworkers = $page->coworkers()->toStructure()) : ?>
+						<?php snippet('coworkers', ['coworkers' => $coworkers]) ?>
+					<?php endif ?>
+				</div>
+			</div>
 
-		<div class="font-sans text-base px-3 pt-2 pb-5">
-			<?php snippet('subtitle', ['subtitle' => 'Ehemalige']) ?>
-			<?= $page->oldcoworkers() ?>
-		</div>
+			<div class="font-sans text-base px-3 pt-2 pb-5">
+				<?php snippet('subtitle', ['subtitle' => 'Ehemalige']) ?>
+				<?= $page->oldcoworkers() ?>
+			</div>
 
-		<div class="font-sans text-base px-3 pt-2 pb-5">
-			<?php snippet('subtitle', ['subtitle' => $page->applicationsTitle()->escape()]) ?>
-			<?php if ($pages->get('jobs')->children()->listed()->count() > 0) : ?>
-				<?php snippet('link', [
-					'url' => "/jobs",
-					'description' => $page->OpenPositionsLinkText()->escape(),
-					'blank' => true
-				]) ?>
-			<?php else : ?>
-				<p><?= $page->noOpenPositions()->escape() ?></p>
-			<?php endif ?>
-		</div>
+			<div class="font-sans text-base px-3 pt-2 pb-5">
+				<?php snippet('subtitle', ['subtitle' => $page->applicationsTitle()->escape()]) ?>
+				<?php if ($pages->get('jobs')->children()->listed()->count() > 0) : ?>
+					<?php snippet('link', [
+						'url' => "/jobs",
+						'description' => $page->OpenPositionsLinkText()->escape(),
+						'blank' => true
+					]) ?>
+				<?php else : ?>
+					<p><?= $page->noOpenPositions()->escape() ?></p>
+				<?php endif ?>
+			</div>
 
-		<div class="text-xs font-mono px-3 pt-2 pb-5">
-			<?php snippet('copyright') ?>
+			<div class="text-xs font-mono px-3 pt-2 pb-5">
+				<?php snippet('copyright') ?>
+			</div>
 		</div>
 	</main>
 
