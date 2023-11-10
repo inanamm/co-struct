41e5ab2e38457d9fa74ff3419576dad1a0d3a9b9
Guido Offermans <dev@guidooffermans.com>
2023-11-10 21:25:10 +0100

    edit backup script

--- scripts/backup.php
+++ scripts/backup.php
@@ -2,7 +2,7 @@
 
 echo "Current working directory: " . getcwd() . "\n";
 
-define('BACKUP_RETENTION_COUNT', 60); // Number of backup copies to retain
+define('BACKUP_RETENTION_COUNT', 3); // Number of backup copies to retain
 
 function backupFolder($sourceDirectory, $destinationDirectory)
 {
@@ -10,43 +10,81 @@ function backupFolder($sourceDirectory, $destinationDirectory)
     $dir = opendir($sourceDirectory);
 
     // Make the destination directory if not exist
-    @mkdir($destinationDirectory);
+    if (!file_exists($destinationDirectory)) {
+        mkdir($destinationDirectory, 0777, true);
+    }
 
     // Loop through the files in source directory
     while ($file = readdir($dir)) {
         if ($file != '.' && $file != '..') {
-            if (is_dir($sourceDirectory . '/' . $file)) {
+            $sourcePath = $sourceDirectory . '/' . $file;
+            $destinationPath = $destinationDirectory . '/' . $file;
+
+            if (is_dir($sourcePath)) {
                 // Recursively backup subdirectories
-                backupFolder($sourceDirectory . '/' . $file, $destinationDirectory . '/' . $file);
+                backupFolder($sourcePath, $destinationPath);
             } else {
                 // Copy files
-                copy($sourceDirectory . '/' . $file, $destinationDirectory . '/' . $file);
+                copy($sourcePath, $destinationPath);
             }
         }
     }
     closedir($dir);
 }
 
+function deleteDirectory($dir)
+{
+    if (!file_exists($dir)) {
+        return true;
+    }
+
+    if (!is_dir($dir)) {
+        return unlink($dir);
+    }
+
+    foreach (scandir($dir) as $item) {
+        if ($item == '.' || $item == '..') {
+            continue;
+        }
+
+        if (!deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) {
+            return false;
+        }
+    }
+
+    return rmdir($dir);
+}
+
 function deleteOldBackups($backupDirectory)
 {
-    // Delete old backups
-    $backups = glob($backupDirectory . '/backup_*');
+    echo "Looking for backups in: $backupDirectory\n";
+
+    $backups = glob($backupDirectory . '/co_struct_backup_*');
+    echo "Found backups:\n";
+    print_r($backups);
+
     if (count($backups) > BACKUP_RETENTION_COUNT) {
         asort($backups); // Sort backups by date
         $backupsToDelete = array_slice($backups, 0, count($backups) - BACKUP_RETENTION_COUNT);
         foreach ($backupsToDelete as $backup) {
-            array_map('unlink', glob($backup . '/*'));
-            rmdir($backup);
+            echo "Deleting backup: $backup\n";
+            deleteDirectory($backup);
         }
     }
 }
 
-// Usage
-$sourceFolder = '/path/to/source/folder';
-$backupFolder = '/path/to/backup/folder_' . date('Y-m-d_H-i-s');
 
-// backupFolder($sourceFolder, $backupFolder);
-// deleteOldBackups($sourceFolder);
+// Absolute path to the root directory for all backups
+$backupRoot = '../../customBackups';
+
+// Source folder to backup
+$sourceFolder = './content';
+
+// Backup folder name
+$backupFolderName = 'co_struct_backup_' . date('Y-m-d_H:i:s');
+$backupFolder = $backupRoot . '/' . $backupFolderName . '/' . basename($sourceFolder);
+
+backupFolder($sourceFolder, $backupFolder);
+deleteOldBackups($backupRoot);
 
 echo "Backup of '$sourceFolder' completed to '$backupFolder' \n\n";
-
