ec2c641735a8e93f9e3f81fbe388742ee661ec9d
Guido Offermans <dev@guidooffermans.com>
2024-04-04 12:42:36 +0200

    display timeframe data in the right way

--- site/templates/about.php
+++ site/templates/about.php
@@ -7,8 +7,7 @@
 	<div class="header bg-cswhite pb-5">
 		<?php snippet('header', slots: true) ?>
 		<?php slot('dash') ?>
-		<div id="dash" class="w-7 bg-csblue h-[0.26rem] lg:h-[0.40rem] mt-[0.30rem] lg:mt-[0.44rem] self-center"
-			alt="logo"></div>
+		<div id="dash" class="w-7 bg-csblue h-[0.26rem] lg:h-[0.40rem] mt-[0.30rem] lg:mt-[0.44rem] self-center" alt="logo"></div>
 		<?php endslot('dash') ?>
 		<?php slot('struct') ?>
 		<?= url('struct-csblue.svg') ?>
@@ -31,7 +30,7 @@
 		<div id="four" class="lg:w-1/2 lg:divide-none divide-y divide-csblue no-scrollbar">
 			<div class="flex flex-col lg:flex-row gap-4 font-sans text-lg px-3 pt-2 pb-5 lg:pb-6 lg:px-0 lg:pr-3">
 				<div class="flex flex-col gap-4">
-					<?php if ($address = $site->addressOffice()->toObject()): ?>
+					<?php if ($address = $site->addressOffice()->toObject()) : ?>
 						<?php snippet(
 							'address',
 							[
@@ -63,7 +62,7 @@
 			<!-- ADRESSEN -->
 			<div class="flex flex-col lg:flex-row font-sans text-lg px-3 pt-2 pb-5 lg:gap-4 lg:pb-6 lg:px-0 lg:pr-3">
 				<div class="flex flex-col gap-4">
-					<?php if ($address = $site->addressZH()->toObject()): ?>
+					<?php if ($address = $site->addressZH()->toObject()) : ?>
 						<?php snippet(
 							'address',
 							[
@@ -76,7 +75,7 @@
 						) ?>
 					<?php endif ?>
 
-					<?php if ($address = $site->addressVD()->toObject()): ?>
+					<?php if ($address = $site->addressVD()->toObject()) : ?>
 						<?php snippet(
 							'address',
 							[
@@ -100,13 +99,13 @@
 				</div>
 
 				<div class="flex flex-col w-full">
-					<?php if ($coworkers = $page->coworkers()->toStructure()): ?>
+					<?php if ($coworkers = $page->coworkers()->toStructure()) : ?>
 						<?php snippet('coworkers', ['coworkers' => $coworkers]) ?>
 					<?php endif ?>
 				</div>
 			</div>
 
-			<?php if ($page->oldcoworkers()->list()->isNotEmpty()): ?>
+			<?php if ($page->oldcoworkers()->list()->isNotEmpty()) : ?>
 				<div class="flex flex-col lg:flex-row lg:gap-4 font-sans text-base px-3 pt-2 pb-5 lg:pb-6 lg:px-0 lg:pr-3">
 					<div class="lg:w-[35%]">
 						<?php snippet('subtitle', ['subtitle' => $page->oldcoworkersTitle()->smartypants()]) ?>
@@ -122,13 +121,13 @@
 					<?php snippet('subtitle', ['subtitle' => $page->applicationsTitle()->smartypants()]) ?>
 				</div>
 				<div class="lg:w-full">
-					<?php if ($pages->get('jobs')->children()->listed()->count() > 0): ?>
+					<?php if ($pages->get('jobs')->children()->listed()->count() > 0) : ?>
 						<?php snippet('link', [
 							'url' => "/jobs",
 							'description' => $page->OpenPositionsLinkText()->smartypants(),
 							'blank' => false
 						]) ?>
-					<?php else: ?>
+					<?php else : ?>
 						<p>
 							<?= $page->noOpenPositions()->smartypants() ?>
 						</p>
@@ -140,7 +139,7 @@
 			<div class="flex flex-col lg:flex-row lg:gap-4 font-sans text-base px-3 pt-2 pb-5 lg:pb-6 lg:px-0 lg:pr-3">
 				<div class="flex-none lg:w-[35%]">
 					<?php snippet('subtitle', ['subtitle' => $page->worklistTitle()->smartypants()]) ?>
-						<!-- <?php var_dump($pages->get('projects')->content()->timeframe()) ?> -->
+					<!-- <?php var_dump($pages->get('projects')->content()->timeframe()) ?> -->
 				</div>
 				<div class="flex flex-col text_with_link font-mono text-sm lg:text-base lg:pr-5">
 
@@ -148,14 +147,18 @@
 					$projectsPage = $pages->get('projects');
 					$projects = $projectsPage->children(); ?>
 
-					<?php foreach ($projects as $project): ?>
+					<?php foreach ($projects as $project) : ?>
 						<?php
 						$title = $project->listTitle();
-						$timeframe = $project->information()->projectDetails()->timeframe()->value();
 						$url = $project->url();
 						?>
-						<a href=><?= $title ?></a>
-						<?= $timeframe ?>
+
+						<a href=<?= $url ?>><?= $title ?></a>
+						<?php foreach ($project->information()->toStructure() as $projectDetail) : ?>
+							<?php if ($projectDetail->projectDetails()->value() == "timeframe") : ?>
+								<p class=""><?= $projectDetail->value() ?></p>
+							<?php endif ?>
+						<?php endforeach ?>
 					<?php endforeach ?>
 					<!-- <div class="flex flex-col text_with_link font-mono text-sm lg:text-base pt-5 pb-5 lg:pr-5">
 						<h3>
