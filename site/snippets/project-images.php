a3bed2161209fc03835846bd35e0fcd8063655bf
Guido Offermans <dev@guidooffermans.com>
2026-04-21 20:27:17 +0200

    filter images

--- site/snippets/project-images.php
+++ site/snippets/project-images.php
@@ -6,21 +6,24 @@
  * @var Kirby\Cms\Pages $pages
  */
 
-$projects = $pages->get('projects')->children();
+$projects = $pages->get('projects')->children()->listed()->filter(function ($page) {
+  return $page->toggle_detailpage()->toBool();
+});
+
 $filteredProjects = param("filter")
-    ? $projects->filterBy('competencies', param("filter"), '*=')
-    : $projects;
+  ? $projects->filterBy('competencies', param("filter"), '*=')
+  : $projects;
 
 $images = [];
 foreach ($filteredProjects as $project) {
-    foreach ($project->gallery()->toFiles() as $image) {
-        if ($image->show_on_landing()->isNotEmpty() && $image->show_on_landing()->toBool() === true) continue;
-        $images[] = (object)[
-            'file'  => $image,
-            'title' => $project->title(),
-            'url'   => $project->url(),
-        ];
-    }
+  foreach ($project->gallery()->toFiles() as $image) {
+    if ($image->show_on_landing()->isNotEmpty() && $image->show_on_landing()->toBool() === true) continue;
+    $images[] = (object)[
+      'file' => $image,
+      'title' => $project->title(),
+      'url' => $project->url(),
+    ];
+  }
 }
 
 shuffle($images);
@@ -30,8 +33,8 @@ $snippetId = $snippetId ?? 'project-images';
 <div id="<?= $snippetId ?>" class="contents">
   <?php foreach ($images as $image): ?>
     <a href="<?= $image->url ?>" class="pb-5 hover:text-cslightblue hover:brightness-105 w-full h-full">
-        <?= $image->file->thumb(['quality' => 30, 'lazy' => true, 'format' => 'webp'])->html() ?>
-        <p class="font-mono text-xs w-full"><?= $image->title ?></p>
+      <?= $image->file->thumb(['quality' => 30, 'lazy' => true, 'format' => 'webp'])->html() ?>
+      <p class="font-mono text-xs w-full"><?= $image->title ?></p>
     </a>
   <?php endforeach ?>
 </div>
